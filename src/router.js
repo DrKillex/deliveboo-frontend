71a53cdf50ff4a3b1bcf99e3f42071910d9373f6
DavideAtzo <atzo9477@gmail.com>
2023-06-15 16:55:27 +0200

    fix form restaurant

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import AppHome from './pages/AppHome.vue';
 import AppAbout from './pages/AppAbout.vue';
-import AppRestaurant from './pages/AppRestaurant.vue';
+import AppRestaurant from './pages/AppRestaurants.vue';
 import PageNotFound from './pages/PageNotFound.vue';
 const router = createRouter({
     history: createWebHistory(),
