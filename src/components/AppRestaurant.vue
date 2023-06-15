67adaca729060ed068a22da8e2a3028d6e5b61be
DavideAtzo <atzo9477@gmail.com>
2023-06-15 11:49:44 +0200

    input e v-model

---
+++ src/components/AppRestaurant.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+    name: 'AppRestaurant',
+    data() {
+        return {
+            restaurant: '',
+            address: '',
+            description: '',
+            telephone: '',
+            email: '',
+            opening_hours: '',
+            p_iva: '',
+            user_id: '',
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome ristorante:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci nome ristorante" v-model="restaurant">
+            </div>
+            <div class="mb-3">
+                <label for="address" class="form-label">Indirizzo ristorante:</label>
+                <input type="text" class="form-control" id="address" name="address"
+                    placeholder="Inserisci indirizzo ristorante" v-model="address">
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione ristorante:</label>
+                <textarea class="form-control" id="description" name="description" rows="4" v-model="description"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="telephone" class="form-label">Inserisci numero di telefono:</label>
+                <input type="text" class="form-control" id="telephone" name="telephone"
+                    placeholder="Inserisci numero di telefono" v-model="telephone">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email ristorante:</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="inserisci email" v-model="email">
+            </div>
+            <div class="mb-3">
+                <label for="p_iva" class="form-label">Partita Iva:</label>
+                <input type="text" class="form-control" id="p_iva" name="p_iva" placeholder="Inserisci partita Iva" v-model="p_iva">
+            </div>
+            <div class="mb-3">
+                <label for="opening_hours" class="form-label">Orario d'apertura:</label>
+                <input type="text" class="form-control" id="opening_hours" name="opening_hours"
+                    placeholder="Inserisci orario d'apertura" v-model="opening_hours">
+            </div>
+            <div class="mb-3">
+                <label for="img" class="form-label">Immagine ristorante:</label>
+                <input class="form-control" type="file" id="img" name="img">
+            </div>
+            <div class="mb-3">
+                <label for="user_id" class="form-label">User id:</label>
+                <input type="text" class="form-control" id="user_id" name="user_id" placeholder="Inserisci user id" v-model="user_id">
+            </div>
+        </div>
+    </div>
+</template>
+ 
+<style lang="scss" scoped></style>
