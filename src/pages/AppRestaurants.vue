f0568af05d44fa1fdb9dd7cdd44247aeacb68da9
DrKillex <lcollaro1994@gmail.com>
2023-06-19 17:56:34 +0200

    fix

--- src/pages/AppRestaurants.vue
+++ src/pages/AppRestaurants.vue
@@ -8,16 +8,30 @@ export default {
     data() {
         return {
             store,
-            restaurants: ""
+            restaurants: "",
+            categories: "",
         }
     },
     methods: {
         getData() {
             axios.get(this.store.apiBaseUrl + this.store.apiUrls.restaurants)
                 .then((response) => {
-                    console.log(response.data.results);
-                    this.restaurants = response.data.results
-                })
+                    console.log(response)
+                    this.restaurants = response.data.results.restaurants
+                    this.categories = response.data.results.categories
+                    // let test=[]
+                    // for (let i = 0; i < this.restaurants.length; i++) {
+                    //     const category = this.restaurants[i].categories
+                    //     for (let index = 0; index < category.length; index++) {
+                    //         const categoryId = category[index].id
+                    //         if (!test.includes(categoryId)) {
+                    //             test.push(categoryId)
+                    //             this.categories.push(category[index])
+                    //         }
+                    //     }
+                    // }
+                }
+            )
         }
     },
     created() {
@@ -30,6 +44,7 @@ export default {
 <template>
     <section class="container">
         <h1>Ristoranti</h1>
+        <div v-for="category in categories">{{ category.name }}</div>
         <div class="d-flex gap-4 flex-wrap">
             <div v-for="restaurant in restaurants" class="card" style="width: 18rem;">
                 <img :src="restaurant.img" class="card-img-top" alt="...">
@@ -38,6 +53,7 @@ export default {
                     <p class="card-text">{{ restaurant.description }}</p>
                     <div>Telefono: {{ restaurant.telephone }} </div>
                     <div>P Iva: {{ restaurant.p_iva }} </div>
+                    <span class="ir-badge me-1" v-for="category in restaurant.categories">{{ category.name }}</span>
                     <div>Email: {{ restaurant.email }} </div>
                     <div>Orario: {{ restaurant.opening_hours }} </div>
                     <div>Indirizzo: {{ restaurant.address }} </div>
