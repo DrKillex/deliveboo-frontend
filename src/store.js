772bd004ac93fb50f185137fa1dbf2504ec7da03
DrKillex <lcollaro1994@gmail.com>
2023-06-22 16:02:22 +0200

    menu

--- src/store.js
+++ src/store.js
@@ -3,12 +3,15 @@ import { reactive } from "vue";
 export const store = reactive({
   apiBaseUrl: import.meta.env.VITE_BACKEND_API_URL,
   apiUrls: {
-    restaurants: '/restaurants'
+    restaurants: '/restaurants',
+    products: '/products'
   },
+  selectedRestaurant: '',
   restaurants: [],
   categories: [],
   categoriesId: [],
   searchBarText: "",
-  selectedCategories: []
+  selectedCategories: [],
+  products: []
 }
 )
