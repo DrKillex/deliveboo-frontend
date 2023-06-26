960e57dcf75406edc6159f6d1bb27653036b50d8
alessandroiero <alessandro.iero@outlook.it>
2023-06-26 12:17:59 +0200

    Font Awesome + Header Nav + Start Cart Component

---
+++ src/components/AppCart.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "AppCart"
+}
+</script>
+
+
+<template>
+    <div class="order-sm-1 order-md-3 col-sm-12 col-md-4 col-lg-3">
+        <!-- Offcanvas -->
+        <button class="btn border-0 fs-6 p-0 border-dark-subtle mx-2" type="button" data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><font-awesome-icon
+                icon="fa-solid fa-cart-shopping" /></button>
+
+        <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
+            aria-labelledby="offcanvasWithBothOptionsLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">I Miei Ordini</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <!-- Form dentro Offcanvas -->
+            <div class="offcanvas-body" id="offcanvas">
+                <form class="row g-3">
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Ordina</button>
+                        <button type="submit" class="btn btn-warning">Svuota Carrello</button>
+                    </div>
+                </form>
+            </div>
+            <!-- Form dentro Offcanvas -->
+        </div>
+        <!-- /Offcanvas -->
+    </div>
+</template>
+
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import { createApp } from 'vue'
 import { router } from './router';
 import './assets/scss/main.scss';
+import "@fontsource/montserrat"; // Defaults to weight 400
 import * as bootstrap from 'bootstrap';
+// /* import the fontawesome core /
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons - inserire qui le icone */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'; 
+/* add icons to the library - inserire qui le icone */
+library.add(faUserSecret, faCartShopping)
 
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
